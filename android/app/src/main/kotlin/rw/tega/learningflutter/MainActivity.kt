0d3767cc4380dfca24b0c750cbcee10d06179071
Ngeyen Victory <ngeyennv@gmail.com>
2023-02-16 10:41:06 +0200

    init flutter learn

---
+++ android/app/src/main/kotlin/rw/tega/learningflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package rw.tega.learningflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+const names = ['Foo', 'Bar', 'Baz'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NameCubit extends Cubit<String?> {
+  NameCubit() : super(null);
+
+  void pickRandomName() => emit(
+        names.getRandomElement(),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NameCubit cubit;
+  @override
+  void initState() {
+    super.initState();
+    cubit = NameCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = TextButton(
+              onPressed: () {
+                cubit.pickRandomName();
+              },
+              child: const Text('Pick a random name'));
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [Text(snapshot.data ?? ''), button],
+              );
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
+  }
+}
